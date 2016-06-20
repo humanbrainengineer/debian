54e1f44fff59ca4d2997ac625f90f822009075b7
Tei shimei <apacheindex@yahoo.com>
2016-06-20 20:35:03 +0800

    Create software.sh

---
+++ software.sh
@@ -0,0 +1,11 @@
+system software :
+#-------------------------------------------------------------------------
+Sublime Text 3
+
+download:
+Ubuntu 64 bit - also available as a tarball for other Linux distributions.
+
+use Gdebi to install the soft ware.
+
+
+#-------------------------------------------------------------------------
